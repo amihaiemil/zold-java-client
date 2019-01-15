795d20c9c15a6f6bcf372feaafb569f5c0eb26b4
Mihai A <amihaiemil@gmail.com>
2019-01-15 13:26:28 +0200

    javadocs

--- src/main/java/com/amihaiemil/zold/UserAgentRequestHeader.java
+++ src/main/java/com/amihaiemil/zold/UserAgentRequestHeader.java
@@ -1,3 +1,28 @@
+/**
+ * Copyright (c) 2019, Mihai Emil Andronache
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1)Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2)Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3)Neither the name of zold-java-client nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.amihaiemil.zold;
 
 import java.io.IOException;
@@ -10,13 +35,13 @@ import org.apache.http.message.BasicHeader;
 
 /**
  * User Agent Request Header Interceptor.
- * @author Boris Kuzmic for docker-java-api project
+ * @author Ammar Atef (a_atef45@yahoo.com)
+ * @version $Id$
  * @since 0.0.1
- * @version 0.0.1
+ * @todo #7:30min We should use this class wherever we are
+ *  instantiating an HttpClient in order to send the User-Agent
+ *  HTTP header.
  */
-/**
-* TODO use this class when instantiating the HttpClient.
-*/
 final class UserAgentRequestHeader extends RequestDefaultHeaders {
 
     /**
